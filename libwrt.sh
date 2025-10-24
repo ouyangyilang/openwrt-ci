6962d35a0e98095dd069c1607e9d0d9ef4e24ff5
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2025-10-24 17:51:42 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,5 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+git clone https://github.com/cokebar/openwrt-vlmcsd.git package/vlmcsd/openwrt-vlmcsd
+git clone https://github.com/cokebar/luci-app-vlmcsd.git package/vlmcsd/luci-app-vlmcsd
